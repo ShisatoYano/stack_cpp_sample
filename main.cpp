e10b977e380b6670f7a6c8d85b7af6804afb8fbe
Shisato <shisatoyano@gmail.com>
2020-12-05 22:48:40 +0900

    implemented stack with c++ stl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_stack(stack<int> s) {
+    while (!s.empty()) {
+        cout << '\t' << s.top();
+        s.pop();
+    }
+    cout << '\n';
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    stack<int> s;
+    s.push(10);
+    s.push(30);
+    s.push(20);
+    s.push(5);
+    s.push(1);
+
+    cout << "Given stack is: ";
+    print_stack(s);
+
+    cout << "\ns.size(): " << s.size();
+    cout << "\ns.top(): " << s.top();
+
+    cout << "\ns.pop(): ";
+    s.pop();
+    print_stack(s);
+
     return 0;
 }
